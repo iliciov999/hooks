d0fd853cba131fc9c92dc3a5e2607f15651c9f12
nichita <iliciov999@mail.ru>
2021-01-21 11:21:20 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>{
+
+    const [ name, setName ] = useState('John');
+    const [ age, setAge ] = useState(25);
+     const [ rate, setRate ] = useState(25);
+
+    return(
+        <div>
+            {name} is {age} years old 
+            his rate is ${rate} per hour
+        </div>
+    );
+
+}; 
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
