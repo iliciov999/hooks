2847f18a1f7b28de058f03d51a4ca5432d45348a
nichita <iliciov999@mail.ru>
2021-01-23 11:08:28 +0200

    commit date

--- src/use-state.js
+++ src/use-state.js
@@ -11,18 +11,16 @@ const App = () => {
 
 const HookSwitcher = () =>{
 
-  const [ color, setColor] = useState('gray');
-  const [ fontSize, setFontSize] = useState(14);
+
   return(
     <div style={{ padding: '10px', 
                   backgroundColor: color, 
                   fontSize: `${fontSize}px`}}>
-                    Hello World
-        <button onClick={ () => setColor('gray')}>Dark</button>
-        <button onClick={ () => setColor('white')}>Light</button>
-    <button  onClick={ () => setFontSize((s) => s + 2)}>+</button>
+        
+        <h2>commit on 23.01.2021</h2>
+
     </div>
-  );
+);
 
 };
 
